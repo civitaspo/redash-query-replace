7491c9021cdee7fe6a5ab008523584a20454d6e0
Civitaspo <civitaspo@gmail.com>
2018-09-03 08:48:52 +0900

    Add logger

--- lib/redash/query/replace.rb
+++ lib/redash/query/replace.rb
@@ -1,9 +1,19 @@
-require "redash/query/replace/version"
+require 'redash/query/replace/version'
+require 'redash/query/replace/logger'
+require 'redash/query/replace/cli'
 
 module Redash
   module Query
     module Replace
-      # Your code goes here...
+
+      def self.logger
+        @logger ||= Logger.new(STDOUT)
+      end
+
+      def self.logger=(logger)
+        @logger = logger
+      end
+
     end
   end
 end
