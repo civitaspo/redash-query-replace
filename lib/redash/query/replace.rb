39e13e5efdb9df8f7d430f1ce423612c5cc0b713
Civitaspo <civitaspo@gmail.com>
2018-09-03 03:19:46 +0900

    bundle gem --exe --coc --no-ext --no-mit --test=rspec redash-query-replace

---
+++ lib/redash/query/replace.rb
@@ -0,0 +1,9 @@
+require "redash/query/replace/version"
+
+module Redash
+  module Query
+    module Replace
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/redash/query/replace/version.rb
@@ -0,0 +1,7 @@
+module Redash
+  module Query
+    module Replace
+      VERSION = "0.1.0"
+    end
+  end
+end
