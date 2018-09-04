f61baa67ea6d9205dfca24ffdc62c0373727478e
Civitaspo <civitaspo@gmail.com>
2018-09-05 08:48:04 +0900

    Impl backup feature into Cli

--- lib/redash/query/replace/cli.rb
+++ lib/redash/query/replace/cli.rb
@@ -19,13 +19,14 @@ module Redash
         class_option :exec, type: :boolean, default: false, dest: 'Run actually. Dry run if this flag is no. (Default: no)'
         class_option :id, type: :numeric, required: false, desc: 'Query id. Either --id or --all option is required (Optional)'
         class_option :all, type: :boolean, default: false, desc: 'The flag that all queries became replacement targets. Either --id or --all option is required. (Default: no)'
+        class_option :backup_dir, type: :string, required: false, desc: 'backup directory (Default: create tmp dir)'
 
         desc "query", "Replace matched string inside query text."
         option :from, type: :string, required: true, desc: 'Replaced target regexp in query text'
         option :to, type: :string, required: true, desc: 'Replacement string'
         def query
           init(options)
-          runner = ReplaceQueryText.new(redash_query_client: redash_client, dry_run: !options[:exec])
+          runner = ReplaceQueryText.new(redash_query_client: redash_client, dry_run: !options[:exec], backup_dir: backup_dir)
           if options[:all]
             runner.replace_all(from: options[:from], to: options[:to])
           else
@@ -71,6 +72,10 @@ module Redash
 
           private def redash_client
             RedashQueryClient.new(redash_url: redash_url, redash_api_key: redash_api_key)
+          end
+
+          private def backup_dir
+            options[:backup_dir] || Dir.mktmpdir
           end
         end
 
